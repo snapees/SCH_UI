11b6e9260a58571ec55bf7e6828b651bed93389c
snapees <iamamitthakur2602@gmail.com>
2025-05-08 20:54:58 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      keepNames: true, // Preserve function and class names
+    },
+  },
   build: {
     rollupOptions: {
       output: {
