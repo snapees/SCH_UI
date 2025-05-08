56017170602e7d32b22380c5bfa1b1e8cdedf26d
snapees <iamamitthakur2602@gmail.com>
2025-05-08 17:23:42 +0530

    removed

---
+++ src/environment.js
@@ -0,0 +1,6 @@
+// http://localhost:5000/api/school/login
+
+// const BASE_API_URL = "http://localhost:5000/api";
+const BASE_API_URL = "https://sms-server-y6n9.onrender.com/api";
+
+export { BASE_API_URL };
